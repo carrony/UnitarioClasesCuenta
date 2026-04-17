c1502fedd8fc7214ba46bec73ed8b7269d45c041
gomez.pahug <gomez.pahug@cordobas.salesianos.edu>
2026-04-17 17:24:59 +0200

    añadidos todos los test relaccionados con la clase CuentaBancaria

--- .idea/test/practica/CuentaBancariaTest.java
+++ .idea/test/practica/CuentaBancariaTest.java
@@ -1,19 +1,27 @@
 package practica;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaBancariaTest {
 
+    private CuentaBancaria cuenta ;
+    private CuentaBancaria cuentaNeg ;
+    @BeforeEach
+    void setUp() {
+        cuenta = new CuentaBancaria("Ana garcia",1000);
+        cuentaNeg = new CuentaBancaria("paco alfajor",-1000);
+        System.out.println("instanciando cuentas...");
+    }
+
     @Test
     void CuentaDelBanco(){
-        CuentaBancaria cuenta= new CuentaBancaria("Ana garcia",1000);
         assertEquals("Ana garcia", cuenta.getTitular());
         assertEquals(1000, cuenta.getSaldo());
         assertFalse(cuenta.isBloqueada());
-        assertEquals(1, CuentaBancaria.getTotalCuentasCreadas());
-        CuentaBancaria cuentaNeg = new CuentaBancaria("paco alfajor",-102);
         assertEquals(0, cuentaNeg.getSaldo());
 
 
@@ -21,8 +29,7 @@ class CuentaBancariaTest {
 
     @Test
     void setTitular() {
-        CuentaBancaria cuenta= new CuentaBancaria("Ana garcia",1000);
-        cuenta.setTitular("paco perez");
+        cuenta.setTitular("pepe perez");
         assertEquals("pepe perez", cuenta.getTitular());
         cuenta.setTitular(null);
         assertEquals("pepe perez", cuenta.getTitular());
@@ -32,13 +39,11 @@ class CuentaBancariaTest {
 
     @Test
     void setSaldo() {
-        CuentaBancaria cuenta= new CuentaBancaria("Ana garcia",1000);
         cuenta.setSaldo(100);
-        assertEquals(100, cuenta.getTitular());
-        cuenta.setSaldo(0);
-        assertEquals(100, cuenta.getTitular());
-        cuenta.setSaldo(-10);
-        assertEquals(100, cuenta.getTitular());
+        assertEquals(100, cuenta.getSaldo());
+        cuenta.setSaldo(-100);
+        assertEquals(100, cuenta.getSaldo());
+
     }
 
     @Test
@@ -60,21 +65,65 @@ class CuentaBancariaTest {
 
     @Test
     void ingresar() {
+        double saldo = cuenta.getSaldo();
+        cuenta.ingresar(1000);
+        assertEquals(saldo+1000,cuenta.getSaldo());
+
+        assertFalse(cuenta.ingresar(-100));
+        assertEquals(saldo+1000,cuenta.getSaldo());
+
+        cuenta.setBloqueada(true);
+        assertFalse(cuenta.ingresar(500));
+        assertEquals(saldo+1000,cuenta.getSaldo());
+
+
     }
 
     @Test
     void retirar() {
+        double saldo = cuenta.getSaldo();
+        assertTrue(cuenta.retirar(200));
+        assertEquals(saldo-200,cuenta.getSaldo());
+
+        assertFalse(cuenta.retirar(-100));
+        assertEquals(saldo-200,cuenta.getSaldo());
+
+        assertFalse(cuenta.retirar(1500));
+        assertEquals(saldo-200,cuenta.getSaldo());
+
+        cuenta.setBloqueada(true);
+        assertFalse(cuenta.retirar(500));
+        assertEquals(saldo-200,cuenta.getSaldo());
     }
 
     @Test
     void aplicarComisionMensual() {
+        assertEquals(998, cuenta.aplicarComisionMensual());
+        assertEquals(998, cuenta.getSaldo());
+        assertEquals(993, cuenta.aplicarComisionMensual());
+        assertEquals(993, cuenta.getSaldo());
+        cuenta.setSaldo(3);
+        assertEquals(0, cuenta.aplicarComisionMensual());
+        assertEquals(0, cuenta.getSaldo());
+        cuenta.setSaldo(3000);
+        cuenta.setBloqueada(true);
+        assertEquals(3000,cuenta.aplicarComisionMensual());
+        assertEquals(3000, cuenta.getSaldo());
     }
-
-    @Test
-    void movimientosValidos() {
-    }
-
     @Test
     void bloquearSiSaldoCero() {
+        cuenta.setSaldo(100);
+        assertFalse(cuenta.bloquearSiSaldoCero());
+        cuenta.setSaldo(0);
+        assertTrue(cuenta.bloquearSiSaldoCero());
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("liberando cuentas...");
+        cuenta=null;
+        cuentaNeg=null;
     }
 }
